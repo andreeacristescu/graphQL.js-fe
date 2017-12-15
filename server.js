d8214e20b61d0cd6e64290e53de894600d21d293
andreea <andreealauracristescu96@gmail.com>
2017-12-15 09:32:14 +0200

    Added PDF Introduction
    Added the Introduction to GraphQl PDF

--- server.js
+++ server.js
@@ -24,3 +24,39 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+
+// example.js
+const express = require('express');
+const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+let port = 3000;
+
+/* Here a simple schema is constructed using the GraphQL schema language (buildSchema).
+   More information can be found in the GraphQL spec release */
+
+let schema = buildSchema(`
+  type Query {
+    postTitle: String,
+    blogTitle: String
+  }
+`);
+
+// Root provides a resolver function for each API endpoint
+let root = {
+    postTitle: () =&gt; {
+    return 'Build a Simple GraphQL Server With Express and NodeJS';
+},
+blogTitle: () =&gt; {
+    return 'scotch.io';
+}
+};
+
+const app = express();
+app.use('/', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true //Set to false if you don't want graphiql enabled
+}));
+
+app.listen(port);
+console.log('GraphQL API server running at localhost:'+ port);
